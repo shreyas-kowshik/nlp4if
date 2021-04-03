19828d1d00fc4af446f081fd0d6bcf9f85619d90
Shreyas <shreyaskowshik@gmail.com>
2021-04-03 20:44:31 +0530

    Add initial embeddings, modify ml model training

--- ml_models_train.py
+++ ml_models_train.py
@@ -2,14 +2,14 @@ import numpy as np
 import pandas as pd
 import random
 import copy
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.ensemble import RandomForestClassifier
-from xgboost import XGBClassifier
-from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 import argparse
 from distutils.util import strtobool
 from utils.preprocess import *
 from utils.train_utils import *
+import os
 import wandb
 
 RANDOM_SEED = 42
@@ -23,18 +23,17 @@ parser.add_argument("-ddp", "--data_dev_path", type=str, default="data/english/v
                     help="Expects a path to dev folder")
 parser.add_argument("-model", "--model_to_use", type=str, default="RandomForest",
                     help="Which model to use")
-parser.add_argument("-max_features_ngram", "--max_features_ngram", type=int, default=5000,
-                    help="Which model to use")                    
-parser.add_argument("-wdbr", "--wandb_run", type=str, required=True,
+parser.add_argument("-wdbr", "--wandb_run", type=str, 
                     help="Wandb Run Name")
 parser.add_argument("-log_to_wnb", "--log_to_wnb", type=strtobool, default=True,
                     help="Wandb Run Name")
-args = parser.parse_args()
+parser.add_argument("-use_emb", "--use_emb", type=strtobool, default=True,
+                    help="Whether to use embeddings")
+parser.add_argument("-embf", "--emb_folder", type=str, default="bert_attn_cw_prepro_head7"
+                    help="Folder Name where Embedding is stored")
 
-# Add initial values here #
-if args.log_to_wnb==True:
-    wandb.init(name=args.wandb_run, project='nlp_runs', entity='nlp4if')
-    wandb.config.update(args)
+
+args = parser.parse_args()
 
 # Set data paths
 TRAIN_FILE=args.data_train_path+"covid19_disinfo_binary_english_train.tsv"
@@ -55,58 +54,61 @@ cw = generate_class_weights(TRAIN_FILE, return_weights=True)
 Y_TRAIN_FULL = convert_label(df_train[label_list].fillna('nan').to_numpy())
 Y_VAL_FULL = convert_label(df_val[label_list].fillna('nan').to_numpy())
 
-X_word_train, X_word_val, X_char_train, X_char_val = build_tfidf_ngrams(df_train, df_val, max_features=args.max_features_ngram)
+
+
+# Generate word and char tfidf ngrams
+word_vectorizer = TfidfVectorizer(
+    sublinear_tf=True,
+    strip_accents='unicode',
+    analyzer='word',
+    token_pattern=r'\w{1,}',
+    stop_words='english',   
+    ngram_range=(1, 2),
+    max_features=5000)
+
+char_vectorizer = TfidfVectorizer(
+    sublinear_tf=True,
+    strip_accents='unicode',
+    analyzer='char',
+    ngram_range=(2, 3),
+    max_features=5000)
+
+char_vectorizer.fit(df_train['text_cleaned'])
+X_char_train = char_vectorizer.transform(df_train['text_cleaned']).toarray()
+X_char_val = char_vectorizer.transform(df_val['text_cleaned']).toarray()
+
+word_vectorizer.fit(df_train['text_cleaned'])
+X_word_train = word_vectorizer.transform(df_train['text_cleaned']).toarray()
+X_word_val = word_vectorizer.transform(df_val['text_cleaned']).toarray()
 
 # Stack word tfidf, char tfidf, hand-crafted features
 X_train = np.hstack([X_char_train, X_word_train, df_train[special_features].to_numpy()])
 X_val = np.hstack([X_char_val, X_word_val, df_val[special_features].to_numpy()])
 
-model_dict = {'RandomForest':RandomForestClassifier, 'XGBoost':XGBClassifier, 'LogisticRegression':LogisticRegression}
-clf, y_pred_train, y_pred_val = train_ml(model_dict[args.model_to_use],  X_train, Y_TRAIN_FULL, X_val, cw)
+# Embedding features
+if args.use_emb:
+    train_emb = np.load(os.path.join('data/', args.emb_folder, 'train_emb.npy'))
+    val_emb = np.load(os.path.join('data/', args.emb_folder, 'val_emb.npy'))
+    X_train = np.hstack([X_train, train_emb])
+    X_val = np.hstack([X_val, val_emb])
+    print("Embeddings stacked!")
 
-print('----------------------- TRAIN SCORES -----------------------')
-train_scores = evaluate_model_ml(y_pred_train.astype(np.int64), y_pred_train.astype(np.int64))
-display_metrics(train_scores)
-print('----------------------- VAL SCORES -----------------------')
-scores, val_preds, val_gt = evaluate_model_ml(y_pred_val.astype(np.int64), Y_VAL_FULL.astype(np.int64), return_files=True)
-display_metrics(scores)
 
-# Save summary wandb
-if args.log_to_wnb==True:
-    wandb.run.summary['Validation Mean F1-Score'] = np.mean(scores['f1'])
-    wandb.run.summary['Validation Accuracy'] = np.mean(scores['acc'])
-    wandb.run.summary['Validation Mean Precision'] = np.mean(scores['p_score'])
-    wandb.run.summary['Validation Mean Recall'] = np.mean(scores['r_score'])
-    wandb.run.summary['Validation Q1 F1 Score'] = scores['f1'][0]
-    wandb.run.summary['Validation Q2 F1 Score'] = scores['f1'][1]
-    wandb.run.summary['Validation Q3 F1 Score'] = scores['f1'][2]
-    wandb.run.summary['Validation Q4 F1 Score'] = scores['f1'][3]
-    wandb.run.summary['Validation Q5 F1 Score'] = scores['f1'][4]
-    wandb.run.summary['Validation Q6 F1 Score'] = scores['f1'][5]
-    wandb.run.summary['Validation Q7 F1 Score'] = scores['f1'][6]
-    wandb.run.summary['Validation Q1 F1 Precision'] = scores['p_score'][0]
-    wandb.run.summary['Validation Q2 F1 Precision'] = scores['p_score'][1]
-    wandb.run.summary['Validation Q3 F1 Precision'] = scores['p_score'][2]
-    wandb.run.summary['Validation Q4 F1 Precision'] = scores['p_score'][3]
-    wandb.run.summary['Validation Q5 F1 Precision'] = scores['p_score'][4]
-    wandb.run.summary['Validation Q6 F1 Precision'] = scores['p_score'][5]
-    wandb.run.summary['Validation Q7 F1 Precision'] = scores['p_score'][6]
+if args.model_to_use=='RandomForest':
+    y_pred_train, y_pred_val = [], []
+    for i in range(7):
+        Y_train = Y_TRAIN_FULL[:,i].astype('int')
+        clf = RandomForestClassifier(class_weight={n:j for n, j in enumerate(cw[i])}, random_state=42)
+        clf.fit(X_train, Y_train)
+        y_pred_train.append(clf.predict(X_train))
+        y_pred_val.append(clf.predict(X_val))
+    
+    y_pred_train=np.vstack(y_pred_train).T
+    y_pred_val=np.vstack(y_pred_val).T
 
-    wandb.run.summary['Train Mean F1-Score'] = np.mean(train_scores['f1'])
-    wandb.run.summary['Train Accuracy'] = np.mean(train_scores['acc'])
-    wandb.run.summary['Train Mean Precision'] = np.mean(train_scores['p_score'])
-    wandb.run.summary['Train Mean Recall'] = np.mean(train_scores['r_score'])
-    wandb.run.summary['Train Q1 F1 Score'] = train_scores['f1'][0]
-    wandb.run.summary['Train Q2 F1 Score'] = train_scores['f1'][1]
-    wandb.run.summary['Train Q3 F1 Score'] = train_scores['f1'][2]
-    wandb.run.summary['Train Q4 F1 Score'] = train_scores['f1'][3]
-    wandb.run.summary['Train Q5 F1 Score'] = train_scores['f1'][4]
-    wandb.run.summary['Train Q6 F1 Score'] = train_scores['f1'][5]
-    wandb.run.summary['Train Q7 F1 Score'] = train_scores['f1'][6]
-    wandb.run.summary['Train Q1 F1 Precision'] = train_scores['p_score'][0]
-    wandb.run.summary['Train Q2 F1 Precision'] = train_scores['p_score'][1]
-    wandb.run.summary['Train Q3 F1 Precision'] = train_scores['p_score'][2]
-    wandb.run.summary['Train Q4 F1 Precision'] = train_scores['p_score'][3]
-    wandb.run.summary['Train Q5 F1 Precision'] = train_scores['p_score'][4]
-    wandb.run.summary['Train Q6 F1 Precision'] = train_scores['p_score'][5]
-    wandb.run.summary['Train Q7 F1 Precision'] = train_scores['p_score'][6]
+    print('----------------------- TRAIN SCORES -----------------------')
+    train_scores = evaluate_model_ml(y_pred_train.astype(np.int64), y_pred_train.astype(np.int64))
+    display_metrics(train_scores)
+    print('----------------------- VAL SCORES -----------------------')
+    scores, val_preds, val_gt = evaluate_model_ml(y_pred_val.astype(np.int64), Y_VAL_FULL.astype(np.int64), return_files=True)
+    display_metrics(scores)
