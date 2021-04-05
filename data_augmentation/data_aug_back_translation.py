36865ccfea31c3553a879e444f0fd04fe3902b4e
amsuhane <ayushsuhane99@gmail.com>
2021-04-05 05:11:58 +0000

    Back translation script ready

--- data_augmentation/data_aug_back_translation.py
+++ data_augmentation/data_aug_back_translation.py
@@ -1,11 +1,13 @@
 import argparse
+import pandas as pd
 from transformers import MarianMTModel, MarianTokenizer
 import copy
+import time
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-dtp", "--data_train_path", type=str, default="data/english/v1/v1/",
+parser.add_argument("-dtp", "--data_train_path", type=str, default="../data/english/v1/v1/",
                     help="Expects a path to training folder")
-parser.add_argument("-ddp", "--data_dev_path", type=str, default="data/english/v2/v2/",
+parser.add_argument("-ddp", "--data_dev_path", type=str, default="../data/english/v2/v2/",
                     help="Expects a path to dev folder")
 args = parser.parse_args()
 
@@ -39,18 +41,25 @@ def back_translate(texts, source_lang="en", target_lang="fr"):
 
 def aug_sentence(text):
     augs = {}
+    st=time.time()
     print(' Back Translating es')
     augs['es']=back_translate(text, source_lang="en", target_lang="es")
+    print(time.time()-st)
+    st=time.time()
     print(' Back Translating fr')
     augs['fr']=back_translate(text, source_lang="en", target_lang="fr")
+    print(time.time()-st)
+    st=time.time()
     print(' Back Translating de')
     augs['de']=back_translate(text, source_lang="en", target_lang="de")
+    print(time.time()-st)
     return augs
 
 TRAIN_FILE=args.data_train_path+"covid19_disinfo_binary_english_train.tsv"
 DEV_FILE=args.data_dev_path+"covid19_disinfo_binary_english_dev_input.tsv"
 
 df_train = pd.read_csv(TRAIN_FILE, sep='\t')
+df_train=df_train[:10]
 
 target_model_name = 'Helsinki-NLP/opus-mt-en-ROMANCE'
 target_tokenizer = MarianTokenizer.from_pretrained(target_model_name)
@@ -74,6 +83,6 @@ df_train_es['tweet_text']=augmented_dict['es']
 df_train_fr['tweet_text']=augmented_dict['fr']
 df_train_de['tweet_text']=augmented_dict['de']
 
-df_train_es.to_csv('df_train_es.tsv', sep='\t', index=False)
-df_train_fr.to_csv('df_train_fr.tsv', sep='\t', index=False)
-df_train_de.to_csv('df_train_de.tsv', sep='\t', index=False)
+df_train_es.to_csv('augmented_datasets/df_train_es_test.tsv', sep='\t', index=False)
+df_train_fr.to_csv('augmented_datasets/df_train_fr_test.tsv', sep='\t', index=False)
+df_train_de.to_csv('augmented_datasets/df_train_de_test.tsv', sep='\t', index=False)
