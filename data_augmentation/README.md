3155d8b8463b4b5fd7f26ad7deff9951f7a230f8
amsuhane <ayushsuhane99@gmail.com>
2021-04-06 07:26:47 +0000

    Script for combining all aug data+orignal data

---
+++ data_augmentation/README.md
@@ -0,0 +1,4 @@
+# How to run
+- Run data_aug_back_translation.py
+- Then, run combine_dataframes.py
+- Final dataset is named: final_dataset.tsv
--- data_augmentation/combine_dataframes.py
+++ data_augmentation/combine_dataframes.py
@@ -2,9 +2,9 @@ import pandas as pd
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-dtp", "--data_train_path", type=str, default="../data/english/v1/v1/",
+parser.add_argument("-dtp", "--data_train_path", type=str, default="../data/english/v3/v3/",
                     help="Expects a path to training folder")
-parser.add_argument("-ddp", "--data_dev_path", type=str, default="../data/english/v2/v2/",
+parser.add_argument("-ddp", "--data_dev_path", type=str, default="../data/english/v3/v3/",
                     help="Expects a path to dev folder")
 args = parser.parse_args()
 
@@ -12,6 +12,9 @@ TRAIN_FILE=args.data_train_path+"covid19_disinfo_binary_english_train.tsv"
 DEV_FILE=args.data_dev_path+"covid19_disinfo_binary_english_dev_input.tsv"
 
 df_train = pd.read_csv(TRAIN_FILE, sep='\t')
-df_train_es=pd.read_csv('augmented_datasets/df_train_es.tsv', sep='\t')
-df_train_fr=pd.read_csv('augmented_datasets/df_train_fr.tsv', sep='\t')
-df_train_de=pd.read_csv('augmented_datasets/df_train_de.tsv', sep='\t')
+df_train_es=pd.read_csv('augmented_datasets/aug_from_old_dataset/df_train_es.tsv', sep='\t')
+df_train_fr=pd.read_csv('augmented_datasets/aug_from_old_dataset/df_train_fr.tsv', sep='\t')
+df_train_de=pd.read_csv('augmented_datasets/aug_from_old_dataset/df_train_de.tsv', sep='\t')
+
+df_full=pd.concat([df_train, df_train_de, df_train_es, df_train_fr], ignore_index=True)
+df_full.to_csv('augmented_datasets/final_dataset.tsv', sep='\t')
