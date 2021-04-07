eccebe0972f546ef9d60154ff3b3eade92d62295
amsuhane <ayushsuhane99@gmail.com>
2021-04-07 17:44:53 +0000

    Download model from wandb

--- get_wandb_files.py
+++ get_wandb_files.py
@@ -1,4 +1,21 @@
 import wandb
-api = wandb.Api()
-run = api.run("<entity>/<project>/<run_id>")
-run.file("model-best.h5").download()
+import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-fp", "--file_path", type=str, required=True,
+                    help="Expects a path to wnb model")
+parser.add_argument("-fn", "--file_name", type=str, required=True,
+                    help="Expects name of file to dowload")                    
+args = parser.parse_args()
+
+'''
+ex: args.fp = "nlp4if/nlp_runs/2we87c77", args.fn="ensemble_aug_inverse_weights_roberta_small.pt"
+'''
+
+if not os.path.isfile(args.file_name):
+    api = wandb.Api()
+    run = api.run(args.mp)
+    run.file(args.file_name).download()
+else:
+    print('File already exists')
