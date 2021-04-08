8e762357129afab4b6c5a4f81a53701f6f321ee8
amsuhane <ayushsuhane99@gmail.com>
2021-04-08 04:05:49 +0000

    Minor change in test pred file generation

--- ensemble_test.py
+++ ensemble_test.py
@@ -9,6 +9,8 @@ parser.add_argument("-ddp", "--data_dev_path", type=str, default="data/english/v
                     help="Expects a path to dev folder")
 parser.add_argument("-dtp", "--data_test_path", type=str, default="data/english/test-input/test-input/",
                     help="Expects a path to training folder")
+parser.add_argument("-psn", "--pred_save_name", type=str, default=None,
+                    help="Name to save pred file with")
 parser.add_argument("-bs", "--batch_size", type=int, default=32,
                     help="Batch Size")
 args = parser.parse_args()
@@ -16,8 +18,24 @@ args = parser.parse_args()
 TEST_FILE = args.data_test_path+"covid19_disinfo_binary_english_test_input.tsv"
 DEV_FILE=args.data_dev_path+"covid19_disinfo_binary_english_dev_input.tsv"
 
-MODEL_PATHS = ['ensemble_aug_inverse_weights_roberta_small.pt']
-if len(MODEL_PATHS)==0:
-    print("GIVE MODEL PATHS")
+print('SET retrive_From_wandb paths')
+retrive_from_wandb = [
+    ["nlp4if/nlp_runs/2we87c77", "ensemble_aug_inverse_weights_roberta_small.pt"],
+]
 
-eval_ensemble_test(MODEL_PATHS, TEST_FILE, DEV_FILE, device=torch.device('cuda'), use_glove_fasttext=False)
+print('Using following file paths', retrive_from_wandb)
+if len(retrive_from_wandb)==0:
+    print("GIVE retrive_from_wandb paths")
+
+for fp, fn in retrive_from_wandb:
+    print(f'python get_wandb_files.py -fp {fp} -fn {fn}')
+    os.system(f'python get_wandb_files.py -fp {fp} -fn {fn}')
+
+MODEL_PATHS = [i[1] for i in retrive_from_wandb]
+
+PRED_SAVE_NAME=args.pred_save_name
+if PRED_SAVE_NAME is None:
+    PRED_SAVE_NAME=('_').join([i.split('.')[0] for i in MODEL_PATHS])+".tsv"
+
+print('File will be saved in test_preds/'+PRED_SAVE_NAME)
+eval_ensemble_test(MODEL_PATHS, TEST_FILE, DEV_FILE, PRED_SAVE_NAME, device=torch.device('cuda'), use_glove_fasttext=False)
