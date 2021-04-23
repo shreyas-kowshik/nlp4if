33b1a99f2fc002b82c120fc9558e5fcea0ebd2b2
Shreyas <shreyaskowshik@gmail.com>
2021-04-23 23:32:35 +0530

    Update

--- get_wandb_files.py
+++ get_wandb_files.py
@@ -9,10 +9,6 @@ parser.add_argument("-fn", "--file_name", type=str, required=True,
                     help="Expects name of file to dowload")                    
 args = parser.parse_args()
 
-'''
-ex: args.fp = "nlp4if/nlp_runs/2we87c77", args.fn="ensemble_aug_inverse_weights_roberta_small.pt"
-'''
-
 if not os.path.isfile(args.file_name):
     api = wandb.Api()
     run = api.run(args.file_path)
